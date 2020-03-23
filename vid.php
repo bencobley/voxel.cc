970df3f29567439930bc2f66bcf4062ffed5451c
Oli Thompson <olithompson@rocketmail.com>
2020-03-23 14:57:25 +0000

    auto redirect when video ends

--- vid.php
+++ vid.php
@@ -10,8 +10,15 @@
 <body style="background-color: black;">
     <div>
     <a href="index" type="button" class="close text-white" style="position: absolute; right: 30px; top: 30px;">&times;</a>
-        <video style="  max-width: 100vw !important; height: 100vh !important; margin: 0 auto; display: block; " autoplay="autoplay" controls loop="loop">
+        <video onended="myFunction()" style="  max-width: 100vw !important; height: 100vh !important; margin: 0 auto; display: block; " autoplay="autoplay" controls>
             <source src="https://voxelmedia.s3.eu-west-2.amazonaws.com/HP/video.mp4" type="video/mp4">
         </video>
     </div>
 </body>
+
+<script type='text/javascript'>
+  function myFunction(){
+    window.location.href = "index.php";
+    console.log("ended");
+  }
+</script>
