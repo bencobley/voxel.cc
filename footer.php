f33b34aea3b712c1c79de94625e74c321b11d645
ot316 <olithompson@rocketmail.com>
2020-03-24 11:04:36 +0000

    fix footer

--- footer.php
+++ footer.php
@@ -8,8 +8,8 @@
                 <h3><a href="hub#communityvibes">Hub</a></h3>
             </div>
             <div class="col-lg-5">
-                <h4><a href="starterpack">Starter Pack</a></h4>
                 <h4><a href="starterpack#techspecs">Tech Specs</a></h4>
+                <h4><a href="starterpack#upcoming">Upcoming Releases</a></h4>
                 <h4><a href="starterpack#faq">FAQ</a></h4>
                 <h4><a href="about">About us</a></h4>
             </div>
